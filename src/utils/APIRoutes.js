39bf65048b8a9d2085cd3c335e05c2c68576d152
NonoLeRobot <sannier.renaud@gmail.com>
2024-02-25 21:10:50 +0100

    online

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://my-sharing-base.sanren.fr/backend";
-export const host = "http://localhost:5000/backend";
+export const host = "https://my-sharing-base.sanren.fr/backend";
+// export const host = "http://localhost:5000/backend";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const uploadRoute = `${host}/upload?`;
